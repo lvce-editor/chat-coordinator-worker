bd5681b90ce3b7125c0318bc9153d810979c5bcd
Le Vivilet <levivilet@pm.me>
2026-03-15 17:33:22 +0100

    feature: remove NetworkCommandMap and update commandMap accordingly

--- packages/chat-coordinator-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-coordinator-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,7 @@
 import * as CoordinatorCommands from '../CoordinatorCommands/CoordinatorCommands.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
-import * as NetworkCommandMap from './NetworkCommandMap.ts'
 
 export const commandMap = {
-  ...NetworkCommandMap.networkCommandMap,
   'chatCoordinator.cancelRun': CoordinatorCommands.cancelRun,
   'chatCoordinator.consumeEvents': CoordinatorCommands.consumeEvents,
   'chatCoordinator.createSession': CoordinatorCommands.createSession,
