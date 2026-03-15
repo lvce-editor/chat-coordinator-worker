88487122361405df031278d8a9fa6f0cc4fa88d9
Le Vivilet <levivilet@pm.me>
2026-03-15 17:50:48 +0100

    feature: add mock OpenAPI request handlers to commandMap

--- packages/chat-coordinator-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-coordinator-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,8 +1,20 @@
 import * as CoordinatorCommands from '../CoordinatorCommands/CoordinatorCommands.ts'
 import { getAiSessionTitle } from '../GetAiSessionTitle/GetAiSessionTitle.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
+import { mockOpenApiRequestGetAll } from '../MockOpenApiRequestGetAll/MockOpenApiRequestGetAll.ts'
+import { mockOpenApiRequestReset } from '../MockOpenApiRequestReset/MockOpenApiRequestReset.ts'
+import { mockOpenApiSetHttpErrorResponse } from '../MockOpenApiSetHttpErrorResponse/MockOpenApiSetHttpErrorResponse.ts'
+import { mockOpenApiStreamFinish } from '../MockOpenApiStreamFinish/MockOpenApiStreamFinish.ts'
+import { mockOpenApiStreamPushChunk } from '../MockOpenApiStreamPushChunk/MockOpenApiStreamPushChunk.ts'
+import { mockOpenApiStreamReset } from '../MockOpenApiStreamReset/MockOpenApiStreamReset.ts'
 
 export const commandMap = {
+  'Chat.mockOpenApiRequestGetAll': mockOpenApiRequestGetAll,
+  'Chat.mockOpenApiRequestReset': mockOpenApiRequestReset,
+  'Chat.mockOpenApiSetHttpErrorResponse': mockOpenApiSetHttpErrorResponse,
+  'Chat.mockOpenApiStreamFinish': mockOpenApiStreamFinish,
+  'Chat.mockOpenApiStreamPushChunk': mockOpenApiStreamPushChunk,
+  'Chat.mockOpenApiStreamReset': mockOpenApiStreamReset,
   'ChatCoordinator.cancelRun': CoordinatorCommands.cancelRun,
   'ChatCoordinator.consumeEvents': CoordinatorCommands.consumeEvents,
   'ChatCoordinator.createSession': CoordinatorCommands.createSession,
