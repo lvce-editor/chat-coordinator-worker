fefd8e815da86d135cb2a27c91205f026e958683
Le Vivilet <levivilet@pm.me>
2026-04-19 20:23:06 +0200

    init worker

--- packages/chat-coordinator-worker/src/parts/Listen/Listen.ts
+++ packages/chat-coordinator-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,17 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
+import { LazyTransferMessagePortRpcParent, WebWorkerRpcClient } from '@lvce-editor/rpc'
+import { ChatStorageWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 
 export const listen = async (): Promise<void> => {
   await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
+
+  const s = await LazyTransferMessagePortRpcParent.create({
+    commandMap: {},
+    send(port) {
+      return RendererWorker.sendMessagePortToChatStorageWorker(port)
+    },
+  })
+  ChatStorageWorker.set(s)
 }
