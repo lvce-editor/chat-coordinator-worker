88834644c52538783594f1e12a5622a98b6c2d24
Le Vivilet <levivilet@pm.me>
2026-04-19 20:12:39 +0200

    store result

--- packages/chat-coordinator-worker/src/parts/AiLoop/AiLoop.ts
+++ packages/chat-coordinator-worker/src/parts/AiLoop/AiLoop.ts
@@ -2,11 +2,11 @@
 import type { AiLoopOptions } from '../AiLoopOptions/AiLoopOptions.ts'
 import type { AiLoopResult } from '../AiLoopResult/AiLoopResult.ts'
 import type { ToolCall } from '../ToolCall/ToolCall.ts'
+import { appendChatEvent } from '../AppendChatEvent/AppendChatEvent.ts'
 import { makeAiRequest } from '../MakeAiRequest/MakeAiRequest.ts'
 
 export const aiLoop = async (loopOptions: AiLoopOptions): Promise<AiLoopResult> => {
   const { systemPrompt, url } = loopOptions
-  // TODO
   let toolCalls: readonly ToolCall<any>[] = []
 
   do {
@@ -15,11 +15,22 @@ export const aiLoop = async (loopOptions: AiLoopOptions): Promise<AiLoopResult> 
       toolCalls,
       url,
     })
+
     if (result.type === 'error') {
+      await appendChatEvent({
+        type: 'aiResponseError',
+        value: result.error,
+      })
       return {
         error: result.error,
         type: 'error',
       }
+    } else {
+      await appendChatEvent({
+        toolCalls: result.toolCalls,
+        type: 'aiResponseSuccess',
+        value: result.data,
+      })
     }
     if (result.toolCalls.length === 0) {
       return {
