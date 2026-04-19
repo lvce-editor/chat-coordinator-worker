7be34227cddcfdf5530c16acd42aab8954ea46a9
Le Vivilet <levivilet@pm.me>
2026-04-19 20:18:16 +0200

    fix

---
+++ packages/chat-coordinator-worker/src/parts/GetToolCallResults/GetToolCallResults.ts
@@ -0,0 +1,8 @@
+import type { ToolCall } from '../ToolCall/ToolCall.ts'
+import type { ToolCallResult } from '../ToolCallResult/ToolCallResult.ts'
+
+export const getToolCallResults = async (toolCalls: readonly ToolCall<any>[]): Promise<readonly ToolCallResult[]> => {
+  // TODO
+
+  return []
+}
