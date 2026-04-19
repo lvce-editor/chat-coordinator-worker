f16eacb56733e5bf3e97ddfe1c7b96ce02b226a3
Le Vivilet <levivilet@pm.me>
2026-04-19 19:14:56 +0200

    add event

---
+++ packages/chat-coordinator-worker/src/parts/ChatEventType/ChatEventType.ts
@@ -0,0 +1,1 @@
+export const HandleSubmit = 'handle-submit'
--- packages/chat-coordinator-worker/src/parts/HandleSubmit/HandleSubmit.ts
+++ packages/chat-coordinator-worker/src/parts/HandleSubmit/HandleSubmit.ts
@@ -1,7 +1,22 @@
+import { appendChatEvent } from '../AppendChatEvent/AppendChatEvent.ts'
+import * as ChatEventType from '../ChatEventType/ChatEventType.ts'
+import { getTimeStamp } from '../GetTimeStamp/GetTimeStamp.ts'
+
 export interface SubmitOptions {
+  readonly attachments: readonly any[]
+  readonly id: string
+  readonly role: 'user' | 'assistant'
+  readonly sessionId: string
   readonly text: string
 }
 
-export const handleSubmit = async (event: SubmitOptions): Promise<void> => {
-  // TODO
+export const handleSubmit = async (options: SubmitOptions): Promise<void> => {
+  const { sessionId, text } = options
+
+  await appendChatEvent({
+    sessionId,
+    timestamp: getTimeStamp(),
+    type: ChatEventType.HandleSubmit,
+    value: text,
+  })
 }
