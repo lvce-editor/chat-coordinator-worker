7755f3633b41edb3ec144101c72cfd4696f57637
Le Vivilet <levivilet@pm.me>
2026-04-19 21:39:21 +0200

    fn

--- packages/chat-coordinator-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-coordinator-worker/src/parts/CommandMap/CommandMap.ts
@@ -2,6 +2,7 @@ import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import { handleSubmit } from '../HandleSubmit/HandleSubmit.ts'
 
 export const commandMap = {
+  'ChatCoordinator.handleSubmit': handleSubmit,
   'ChatCoordinator.submit': handleSubmit,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
   initialize: (_: string, port: MessagePort): Promise<void> => handleMessagePort(port),
