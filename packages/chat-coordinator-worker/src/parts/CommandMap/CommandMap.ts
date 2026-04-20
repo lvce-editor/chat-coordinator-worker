4af00333fe5f1e9bee9f5123b6377a3390772337
Le Vivilet <levivilet@pm.me>
2026-04-20 22:14:41 +0200

    mock

--- packages/chat-coordinator-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-coordinator-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,8 +1,10 @@
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import { handleSubmit } from '../HandleSubmit/HandleSubmit.ts'
+import { registerMockResponse } from '../RegisterMockResponse/RegisterMockResponse.ts'
 
 export const commandMap = {
   'ChatCoordinator.handleSubmit': handleSubmit,
+  'ChatCoordinator.registerMockResponse': registerMockResponse,
   'ChatCoordinator.submit': handleSubmit,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
   initialize: (_: string, port: MessagePort): Promise<void> => handleMessagePort(port),
