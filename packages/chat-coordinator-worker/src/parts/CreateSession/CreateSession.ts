aeb242039ebd1a8e1a76c4c23fbb621b2ca1f643
Le Vivilet <levivilet@pm.me>
2026-05-15 23:47:20 +0200

    feature: add title property to CreateSessionOptions and update createSession function (#88)

--- packages/chat-coordinator-worker/src/parts/CreateSession/CreateSession.ts
+++ packages/chat-coordinator-worker/src/parts/CreateSession/CreateSession.ts
@@ -2,14 +2,15 @@ import { ChatStorageWorker } from '@lvce-editor/rpc-registry'
 
 export interface CreateSessionOptions {
   readonly sessionId: string
-  readonly text: string
   readonly timestamp: string
+  readonly title: string
 }
 
 export const createSession = async (options: CreateSessionOptions): Promise<void> => {
-  const { sessionId, timestamp } = options
+  const { sessionId, timestamp, title } = options
   await ChatStorageWorker.invoke('ChatStorage.createSession', {
     sessionId,
     timestamp,
+    title,
   })
 }
